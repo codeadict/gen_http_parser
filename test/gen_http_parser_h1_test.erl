c6298f557bb8aaba703bc3203bf9eaf16c9a9e8e
Dairon Medina Caro <dairon.medina@gmail.com>
2024-09-11 22:43:15 -0400

    Add tests

--- test/gen_http_parser_h1_test.erl
+++ test/gen_http_parser_h1_test.erl
@@ -1,3 +1,33 @@
 -module(gen_http_parser_h1_test).
 
 -include_lib("eunit/include/eunit.hrl").
+
+encode_with_header_test() ->
+    ?assertEqual(
+        <<"GET / HTTP/1.1\r\nfoo: bar\r\n\r\n">>,
+        encode_request("GET", "/", [{"foo", "bar"}], undefined)
+    ).
+
+encode_with_invalid_header_name_test() ->
+    ?assertEqual(
+        {error, {invalid_header_name, <<"f oo">>}},
+        gen_http_parser_h1:encode_request("GET", "/", [{"f oo", "bar"}], undefined)
+    ).
+
+% encode_with_invalid_header_value_test() ->
+%     ?assertEqual(
+%         {error, {invalid_header_value, <<"foo">>, <<"b ar">>}},
+%         gen_http_parser_h1:encode_request("GET", "/", [{"foo", "b ar"}], undefined)
+%     ).
+
+
+encode_request_with_body_test() ->
+    ?assertEqual(
+        <<"GET / HTTP/1.1\r\n\r\nBODY">>,
+        encode_request("GET", "/", [], "BODY")
+    ).
+
+
+encode_request(Method, Target, Headers, Body) ->
+    {ok, IoData} = gen_http_parser_h1:encode_request(Method, Target, Headers, Body),
+    iolist_to_binary(IoData).
